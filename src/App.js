3c2ab648117b234df173fd88dc689d8af5806b2b
Damian Gutierrez Cisneros <d.gutierrez.cisneros@accenture.com>
2020-02-06 17:52:30 +0100

    Crear lista con iconos de colores

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
 import React from 'react';
-import logo from './logo.svg';
+import { List, Avatar } from "antd";
 import './App.css';
 
+const data = [
+  {
+    title: 'Camión de basura 1',
+    description: 'Normalmente usado por la noche',
+    status: 0
+  },
+  {
+    title: 'Barredora 1',
+    description: 'Usada en Málaga',
+    status: 1
+  },
+  {
+    title: 'Camión de basura 2',
+    description: 'Normalmenta usado por de dia',
+    status: 0
+  },
+  {
+    title: 'Camion de Basura 3',
+    description: 'Camión de sustitución',
+    status: 2
+  },
+];
+
 function App() {
+  const [imgName, setImgName] = React.useState("check-green");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <List
+      className="List"
+      itemLayout="horizontal"
+      dataSource={data}
+      renderItem={item => (
+        <List.Item>
+          <List.Item.Meta
+            avatar={<Avatar src={`./icons/${getImgName(item.status)}.png`} />}
+            title={<a href="https://ant.design">{item.title}</a>}
+            description={item.description}
+          />
+        </List.Item>
+      )}
+    />
   );
+}
+
+function getImgName (status) {
+  switch (status) {
+    case 0:
+      return "stop-red"
+    case 1:
+      return "warning-yelow"
+    case 2:
+      return "check-green"
+
+    default:
+      break;
+  }
+  ;
 }
 
 export default App;
